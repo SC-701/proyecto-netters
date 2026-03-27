32a62eccc39e8891b144764fab875318099ab015
NicolasRiveraS <nicolasrivera0306@gmail.com>
2026-03-27 13:43:45 -0600

    3 Reestructuración - Escuela. CRUD de Escuela completado.

---
+++ UniPlan/UniPlan.API/Flujo/EscuelaFlujo.cs
@@ -0,0 +1,33 @@
+﻿using Abstracciones.Interfaces.DA;
+using Abstracciones.Interfaces.Flujo;
+using Abstracciones.Modelos;
+
+namespace Flujo {
+    public class EscuelaFlujo : IEscuelaFlujo {
+        private IEscuelaDA _escuelaDA;
+
+        public EscuelaFlujo (IEscuelaDA escuelaDA) {
+            _escuelaDA = escuelaDA;
+        }
+
+        public async Task<Guid> Agregar (EscuelaRequest escuela) {
+            return await _escuelaDA.Agregar(escuela);
+        }
+
+        public async Task<Guid> Editar (Guid Id, EscuelaRequest escuela) {
+            return await _escuelaDA.Editar(Id, escuela);
+        }
+
+        public async Task<Guid> Eliminar (Guid Id) {
+            return await _escuelaDA.Eliminar(Id);
+        }
+
+        public async Task<IEnumerable<EscuelaResponse>> Obtener () {
+            return await _escuelaDA.Obtener();
+        }
+
+        public async Task<EscuelaResponse> Obtener (Guid Id) {
+            return await _escuelaDA.Obtener(Id);
+        }
+    }
+}
