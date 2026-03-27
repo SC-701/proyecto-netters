e580554a0e3a48458aeb11db48eb351eb6022651
G4b00 <gchavesrojas@gmail.com>
2026-03-26 18:48:47 -0600

    Cambio en la base de datos

--- UniPlan/UniPlan.API/BD/dbo/Tables/Carrera_1.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[Carrera] (
-    [Id]     UNIQUEIDENTIFIER DEFAULT (newid()) NOT NULL,
-    [Nombre] NVARCHAR (150)   NOT NULL,
-    [Activo] BIT              DEFAULT ((1)) NOT NULL,
-    CONSTRAINT [PK_Carrera] PRIMARY KEY CLUSTERED ([Id] ASC)
-);
-
