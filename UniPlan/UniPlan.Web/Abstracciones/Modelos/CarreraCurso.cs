2ccf3437412b8dd870f86855cc75a910a7f2cd0c
G4b00 <gchavesrojas@gmail.com>
2026-03-26 19:03:12 -0600

    Modelos en el Web

---
+++ UniPlan/UniPlan.Web/Abstracciones/Modelos/CarreraCurso.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Abstracciones.Modelos
+{
+    public class CarreraCursoBase
+    {
+        [Required(ErrorMessage = "El Id de la carrera es requerido")]
+        public Guid IdCarrera { get; set; }
+
+        [Required(ErrorMessage = "El Id del curso es requerido")]
+        public Guid IdCurso { get; set; }
+
+        [Required(ErrorMessage = "El cuatrimestre es requerido")]
+        [Range(1, 12, ErrorMessage = "El cuatrimestre debe estar entre 1 y 12")]
+        public int Cuatrimestre { get; set; }
+    }
+
+    public class CarreraCursoRequest : CarreraCursoBase { }
+
+    public class CarreraCursoResponse : CarreraCursoBase
+    {
+        public string Carrera { get; set; }
+        public string Curso { get; set; }
+        public bool Activo { get; set; }
+    }
+}
