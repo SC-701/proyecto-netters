ad6040c7c2d9467335f639ace2729c0ece10c273
Cartin2202 <fabricio.cartin.arias@hotmail.com>
2026-03-30 19:19:41 -0600

    avance controller

---
+++ UniPlan/UniPlan.API/API/Controllers/RequisitosController.cs
@@ -0,0 +1,38 @@
+﻿using Abstracciones.Interfaces.API;
+using Abstracciones.Interfaces.Flujo;
+using Abstracciones.Modelos;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class RequisitosController : Controller, IRequisitosController
+    {
+        private IRequisitosFlujo _requisitosFlujo;
+        private ILogger<RequisitosController> _logger;
+
+        public RequisitosController(IRequisitosFlujo requisitosFlujo, ILogger<RequisitosController> logger)
+        {
+            _requisitosFlujo = requisitosFlujo;
+            _logger = logger;
+        }
+
+        #region CRUD
+
+        [HttpPost]
+        [Authorize(Roles = "2")]
+        public async Task<IActionResult> Agregar([FromBody] RequisitosRequest requisito)
+        {
+            var resultado = await _requisitosFlujo.Agregar(requisito);
+            return CreatedAtAction(nameof(ObtenerPorCurso), new
+            {
+                IdCarrera = resultado.IdCarrera,
+                IdCurso = resultado.IdCurso
+            }, null);
+        }
+
+
+    }
