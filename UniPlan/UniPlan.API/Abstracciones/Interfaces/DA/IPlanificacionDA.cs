c452da09f31c37701aa580105a01d8929f012fc5
Sebasgonzalez26 <147010566+Sebasgonzalez26@users.noreply.github.com>
2026-03-30 21:27:08 -0600

    CRUD COMPLETO DE Planificacion, Flata agregar reglas, pero mas adelante

---
+++ UniPlan/UniPlan.API/Abstracciones/Interfaces/DA/IPlanificacionDA.cs
@@ -0,0 +1,13 @@
+﻿using Abstracciones.Modelos;
+
+namespace Abstracciones.Interfaces.DA
+{
+    public interface IPlanificacionDA
+    {
+        Task<Guid> AgregarPlanificacion(Planificacion planificacion);
+        Task<IEnumerable<PlanificacionResponse>> ObtenerPlanificaciones();
+        Task<IEnumerable<PlanificacionResponse>> ObtenerPlanificacionesPorUsuarioId(Guid idUsuario);
+        Task<Guid> EditarPlanificacion(Guid id, Planificacion planificacion);
+        Task<Guid> EliminarPlanificacion(Guid id);
+    }
+}
